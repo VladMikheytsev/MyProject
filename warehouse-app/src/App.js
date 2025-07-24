d603facf6aa3962fd383bc8e69be81431772717d
Владислав <vladmikheytsev@gmail.com>
2025-07-24 14:43:43 -0700

    обновление App.js

--- warehouse-app/src/App.js
+++ warehouse-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-// --- Иконки (SVG) ---
+// --- Иконки (SVG11111111) ---
 const PlusIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>;
 const EditIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>;
 const ChevronDownIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="6 9 12 15 18 9"></polyline></svg>;
@@ -25,18 +25,19 @@ const api = {
       return null;
     }
   },
-  saveAllData: async (fullState) => {
-    try {
-      await fetch(`${API_BASE_URL}/api/data`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(fullState),
-      });
-      console.log("Данные успешно отправлены на сервер.");
-    } catch (error) {
-      console.error("Ошибка при сохранении данных:", error);
-    }
-  },
+    saveAllData: async (fullState) => {
+        try {
+            const response = await fetch(`${API_BASE_URL}/api/data`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(fullState),
+            });
+            console.log("Ответ от сервера:", await response.json());
+        } catch (error) {
+            console.error("Ошибка при сохранении данных:", error);
+        }
+    },
+
 };
 
 
