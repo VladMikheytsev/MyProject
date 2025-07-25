628bd763c11f2985b42b1a775ad69fd3864be883
Владислав <vladmikheytsev@gmail.com>
2025-07-25 01:39:58 -0700

    обновление Appыыы.js

--- warehouse-app/src/App.js
+++ warehouse-app/src/App.js
@@ -22,7 +22,7 @@ const FilePlusIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="18" he
 const EyeIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>;
 
 
-// --- API Configuration ---
+// --- API Configuration --
 // !!! ВАЖНО: Вставьте сюда ваш актуальный URL от ngrok !!!
 const API_BASE_URL = "https://warehouse-vlad.ngrok.io"; 
 
